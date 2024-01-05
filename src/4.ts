12049a58daff8f3cba363a9569510fddc30f00e6
TaniaUdod <taniaudod28@gmail.com>
2024-01-05 21:53:45 +0200

    Update

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
-// const key = new Key();
+class Key {
+  private signature: number;
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+  constructor() {
+    this.signature = Math.random();
+  }
 
-// house.openDoor(person.getKey());
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-// house.comeIn(person);
+class Person {
+  constructor(private key: Key) {}
 
+  getKey(): Key {
+    return this.key;
+  }
+}
 
-// export {};
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+house.comeIn(person);
+
+export {};
